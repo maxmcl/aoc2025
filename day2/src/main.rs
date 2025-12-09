417708b3d0d01eb276b1b0f375f94b1922d01e38
max <maxime.mcl@gmail.com>
2025-12-08 21:35:52 -0500

    Part 2, oh boy spent too long getting this to work send help

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[derive(Debug)]
 struct Range {
     start: u64,
@@ -5,32 +7,43 @@ struct Range {
 }
 
 #[derive(Debug)]
-struct Half {
-    pub value: u64,
+struct Group {
+    // TODO: pick better names
+    value: u64,
     base: u64,
-    n: u32, // an even number by construction
+    n: u32,
     next_base: u64,
+    size: u32,
+    group_size: u32,
 }
 
-impl Half {
-    fn from_value(value: u64) -> Self {
-        let mut n = Half::get_n_digits(value);
-        let (base, next_base) = if n.is_multiple_of(2) {
-            n /= 2;
-            let next_base = 10_u64.pow(n);
-            (value / next_base, next_base)
+impl Group {
+    fn from(value: u64, size: u32) -> Self {
+        let n_digits = Group::get_n_digits(value);
+        let mut out = if n_digits.is_multiple_of(size) {
+            let n = n_digits / size;
+            Self {
+                value: 0,
+                n,
+                base: value / 10u64.pow(n_digits - n),
+                next_base: 10u64.pow(n),
+                size: n_digits,
+                group_size: size,
+            }
         } else {
-            // Odd value, skip to next even digit number
-            n = n.div_ceil(2);
+            let n = n_digits.div_ceil(size);
             let next_base = (10u64).pow(n);
-            (next_base / 10, next_base)
+            Self {
+                value: 0,
+                base: next_base / 10,
+                next_base,
+                n,
+                size: n * size,
+                group_size: size,
+            }
         };
-        Self {
-            value: Half::compute_value(base, n),
-            base,
-            n,
-            next_base,
-        }
+        out.compute_value();
+        out
     }
 
     fn next(&mut self) {
@@ -39,12 +52,17 @@ impl Half {
             // Skip odd numbers
             self.n += 1;
             self.next_base *= 10;
+            self.size = self.group_size * self.n;
         }
-        self.value = Half::compute_value(self.base, self.n);
+        self.compute_value();
     }
 
-    fn compute_value(base: u64, n: u32) -> u64 {
-        base * 10_u64.pow(n) + base
+    fn compute_value(&mut self) {
+        self.value = self.base
+            * (0..self.size)
+                .step_by(self.n as usize)
+                .map(|p| 10_u64.pow(p))
+                .sum::<u64>()
     }
 
     fn get_n_digits(mut value: u64) -> u32 {
@@ -59,29 +77,22 @@ impl Half {
 
 impl Range {
     fn sum_invalid_ids(&self) -> u64 {
-        /* an invalid ID is made of 2 repeated sequences of digits,
-          therefore, it must have an even length. E.g.
-          22, 6464, 123123
-
-          therefore, since the number must be repeated, only the
-          first half of the numbers in the range can be considered. E.g.
-
-          83-113
-          |   |> length 3, ignored
-          |
-          v length 2, first half = 8, must be >= 8
-          candidates: 88, 99
-        */
-        let mut half = Half::from_value(self.start);
-        while half.value < self.start {
-            half.next();
+        let mut invalid_ids = HashSet::default();
+        for size in 2..=Group::get_n_digits(self.end) {
+            self.get_invalid_ids_for_group(size, &mut invalid_ids);
         }
-        let mut sum = 0;
-        while half.value <= self.end {
-            sum += half.value;
-            half.next();
+        invalid_ids.into_iter().sum()
+    }
+
+    fn get_invalid_ids_for_group(&self, size: u32, invalid_ids: &mut HashSet<u64>) {
+        let mut group = Group::from(self.start, size);
+        while group.value < self.start {
+            group.next();
         }
-        sum
+        while group.value <= self.end {
+            invalid_ids.insert(group.value);
+            group.next();
+        }
     }
 }
 
@@ -112,4 +123,123 @@ fn main() {
         .map(|range| range.sum_invalid_ids())
         .sum::<u64>()
     );
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn test_group_988() {
+        {
+            let mut group = Group::from(998, 2);
+            assert_eq!(group.value, 1010);
+            group.next();
+            assert_eq!(group.value, 1111);
+            group.next();
+            assert_eq!(group.value, 1212);
+        }
+
+        {
+            let mut group = Group::from(998, 3);
+            assert_eq!(group.value, 999);
+            group.next();
+            assert_eq!(group.value, 101010);
+        }
+        {
+            let mut group = Group::from(998, 4);
+            assert_eq!(group.value, 1111);
+            group.next();
+            assert_eq!(group.value, 2222);
+        }
+    }
+
+    #[test]
+    fn test_group_200000() {
+        {
+            let mut group = Group::from(200_000, 2);
+            assert_eq!(group.value, 200_200);
+            group.next();
+            assert_eq!(group.value, 201_201);
+        }
+
+        {
+            let mut group = Group::from(200_000, 3);
+            assert_eq!(group.value, 202_020);
+            group.next();
+            assert_eq!(group.value, 212_121);
+        }
+
+        {
+            let mut group = Group::from(200_000, 4);
+            assert_eq!(group.value, 10_101_010);
+            group.next();
+            assert_eq!(group.value, 11_111_111);
+        }
+
+        {
+            let mut group = Group::from(200_000, 5);
+            assert_eq!(group.value, 1_010_101_010);
+            group.next();
+            assert_eq!(group.value, 1_111_111_111);
+        }
+
+        {
+            let mut group = Group::from(200_000, 6);
+            assert_eq!(group.value, 222_222);
+            group.next();
+            assert_eq!(group.value, 333_333);
+        }
+    }
+
+    #[test]
+    fn test_group_95() {
+        assert_eq!(Group::from(95, 2).value, 99);
+        assert_eq!(Group::from(95, 3).value, 111);
+    }
+
+    #[test]
+    fn test_weird() {
+        let mut group = Group::from(999_999, 2);
+        assert_eq!(group.value, 999_999);
+        group.next();
+        assert_eq!(group.value, 10_001_000);
+    }
+
+    #[test]
+    fn test_range() {
+        assert_eq!(Range { start: 11, end: 22 }.sum_invalid_ids(), 11 + 22);
+        assert_eq!(
+            Range {
+                start: 95,
+                end: 115
+            }
+            .sum_invalid_ids(),
+            99 + 111
+        );
+        assert_eq!(
+            Range {
+                start: 998,
+                end: 1012
+            }
+            .sum_invalid_ids(),
+            999 + 1010
+        );
+        assert_eq!(
+            Range {
+                start: 1188511880,
+                end: 1188511890
+            }
+            .sum_invalid_ids(),
+            1188511885
+        );
+        assert_eq!(
+            Range {
+                start: 2121212118,
+                end: 2121212124
+            }
+            .sum_invalid_ids(),
+            2121212121
+        );
+    }
 }
